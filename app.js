b5777632ef99058b4933e9d8c14a388b9f208702
Sruthik Koneti <sruthikkoneti@gmail.com>
2023-07-17 20:42:37 +0530

    Update app.js

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.set('views',__dirname+"/views")
 app.set('view engine','ejs')
 
 app.use(bodyParser.urlencoded({extended:true}))
-app.use(express.static(__dirname+"public"))
+app.use(express.static(__dirname+"/public"))
 
 
 app.get("/",function(req,res){
